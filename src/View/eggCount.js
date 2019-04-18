7232d4465178d92175517c9ddf7670e522b7eeb6
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-04-18 22:09:11 +0200

    fix

--- src/View/eggCount.js
+++ src/View/eggCount.js
@@ -3,10 +3,10 @@
             count = 0;
     div.onclick = function () {
             count += 1;
-            var audio = new Audio('assets/audio/scary1.mp3');
-            var audio1 = new Audio('assets/audio/scary2.mp3');
-            audio.play();
-            audio1.play();
-            div.innerHTML = "Number of eggs: " + count;
+            //var audio = new Audio('assets/audio/scary1.mp3');
+            //var audio1 = new Audio('assets/audio/scary2.mp3');
+            //audio.play();
+            //audio1.play();
+            div.innerHTML = "Eggs : " + count;
         };
 </script>
