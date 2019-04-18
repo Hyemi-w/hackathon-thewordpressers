24a2e7d6982b8808047bb5ed1b37b327393e9a63
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-04-19 01:31:12 +0200

    compteur à jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -37,50 +37,88 @@ class HomeController extends AbstractController
         $json = $body->getContents();
         $character = json_decode($json, true);
 
-        $eggCount = $_SESSION['eggCount'];
         $title = '';
         $isWinning = '';
         if (strstr($egg['power'], 'increase')) {
             $title = 'Yay !';
-
             $isWinning = 'getting';
         } elseif (strstr($egg['power'], 'decrease')) {
             $title = 'Crap !';
             $isWinning = 'losing';
         }
-
+        $numberDisplay = $_SESSION['eggCount'];
+        $eggCount = $_SESSION['eggCount'];
         $number = 0;
         $size = 0;
         if (strstr($egg['caliber'], 'XS')) {
             $number = 1;
             $size = 'a tiny';
+            if ($isWinning == 'getting') {
+                $eggCount += 1;
+            } else {
+                $eggCount -= 1;
+            }
         } elseif (strstr($egg['caliber'], 'S')) {
             $number = 2;
             $size = 'a small';
+            if ($isWinning == 'getting') {
+                $eggCount += 2;
+            } else {
+                $eggCount -= 2;
+            }
         } elseif (strstr($egg['caliber'], 'M')) {
             $number = 3;
             $size = 'a regular';
+            $eggCount += 3;
+            if ($isWinning == 'getting') {
+                $eggCount += 3;
+            } else {
+                $eggCount -= 3;
+            }
         } elseif (strstr($egg['caliber'], 'L')) {
             $number = 4;
             $size = 'a large';
+            $eggCount += 4;
+            if ($isWinning == 'getting') {
+                $eggCount += 4;
+            } else {
+                $eggCount -= 4;
+            }
         }
 
         if (strstr($egg['caliber'], 'XL')) {
             $number = 5;
             $size = 'an extra large';
+            $eggCount += 5;
+            if ($isWinning == 'getting') {
+                $eggCount += 5;
+            } else {
+                $eggCount -= 5;
+            }
         }
         if (strstr($egg['caliber'], '2XL')) {
             $number = 6;
             $size = 'a huge';
+            $eggCount += 6;
+            if ($isWinning == 'getting') {
+                $eggCount += 6;
+            } else {
+                $eggCount -= 6;
+            }
         }
         if (strstr($egg['caliber'], '3XL')) {
             $number = 7;
             $size = 'an ENORMOUS';
+            $eggCount += 7;
+            if ($isWinning == 'getting') {
+                $eggCount += 7;
+            } else {
+                $eggCount -= 7;
+            }
         }
-
         $_SESSION['eggCount'] = $eggCount;
         return $this->twig->render('Home/index.html.twig', ['egg' => $egg, 'character' => $character,
             'title' => $title, 'isWinning' => $isWinning, 'number' => $number, 'size' => $size,
-            'eggCount' => $eggCount]);
+            'eggCount' => $eggCount, 'numberDisplay' => $numberDisplay]);
     }
 }
--- src/View/eggCount.js
+++ src/View/eggCount.js
@@ -1,4 +1,22 @@
 <script type="text/javascript">
+            //var audio = new Audio('assets/audio/scary1.mp3');
+            //var audio1 = new Audio('assets/audio/scary2.mp3');
+            //audio.play();
+            //audio1.play();
+    var div = document.getElementById("displayEgg");
 
-};
+    var close = document.getElementById('btn');
+    close.onclick = function () {
+        div.innerHTML = 'Eggs : {{ eggCount }}';
+        window.location.reload()
+
+    }
+
+    var close1 = document.getElementById('btn1');
+    close1.onclick = function () {
+            div.innerHTML = 'Eggs : {{ eggCount }}';
+            window.location.reload()
+        }
+
 </script>
+
